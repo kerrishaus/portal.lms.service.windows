301bf07a13f6741b71eca3696b5221aa43ad2f00
Kenny McCormick <kennyrmccormick@gmail.com>
2021-03-17 14:16:59 -0500

    Add project files.

---
+++ windows_service.lms.portal.kunindustries.com/PortalLMSSevice.Designer.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace windows_service.lms.portal.kunindustries.com
+{
+    partial class PortalLMSSevice
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.EntryWritten += new System.Diagnostics.EntryWrittenEventHandler(this.eventLog1_EntryWritten);
+            // 
+            // PortalLMSSevice
+            // 
+            this.AutoLog = false;
+            this.CanHandlePowerEvent = true;
+            this.CanHandleSessionChangeEvent = true;
+            this.ServiceName = "Portal LMS Service";
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+
+        }
+
+        #endregion
+
+        private System.Diagnostics.EventLog eventLog1;
+    }
+}
---
+++ windows_service.lms.portal.kunindustries.com/PortalLMSSevice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace windows_service.lms.portal.kunindustries.com
+{
+    public partial class PortalLMSSevice : ServiceBase
+    {
+        Timer serviceTimer = new Timer();
+
+        String LogSource = "Portal_LMS_Source";
+
+        public PortalLMSSevice()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists(LogSource))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(LogSource, "");
+            }
+            eventLog1.WriteEntry("Portal LMS Service created.");
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Starting Portal LMS Service.");
+            InitialiseTimer();
+            eventLog1.WriteEntry("Started Portal LMS Service.");
+        }
+
+        protected override void OnStop()
+        {
+            serviceTimer.Dispose();
+        }
+
+        private void InitialiseTimer()
+        {
+            try
+            {
+                if (serviceTimer != null)
+                {
+                    serviceTimer.AutoReset = true;
+                    serviceTimer.Interval = Convert.ToDouble(60 * 1000);
+                    serviceTimer.Enabled = true;
+                    serviceTimer.Elapsed += serviceTimer_Elapsed;
+                    eventLog1.WriteEntry("Portal LMS Service Timer Initialized", EventLogEntryType.Information);
+                }
+            }
+            catch (Exception ex)
+            {
+                eventLog1.WriteEntry(ex.Message, EventLogEntryType.Error);
+            }
+        }
+
+        protected void serviceTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //Your Functionality
+            eventLog1.WriteEntry("Elapsed Event Called", EventLogEntryType.Information);
+        }
+
+        private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+
+        }
+    }
+}
